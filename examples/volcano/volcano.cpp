0959c2827c1122d2e62e0eb0b9f8b723c47075bb
Daniel Johansson <ainbusksingers@gmail.com>
2018-12-29 22:42:48 +0100

    Added slang shaders to volcano

--- examples/volcano/volcano.cpp
+++ examples/volcano/volcano.cpp
@@ -91,6 +91,144 @@
 #include <imgui_widgets.cpp>
 #include <examples/imgui_impl_vulkan.cpp>
 
+#include <slang.h>
+
+void loadSlangShaders(std::vector<char>& outVertexShaderData, std::vector<char>& outFragmentShaderData)
+{
+    // First, we need to create a "session" for interacting with the Slang
+    // compiler. This scopes all of our application's interactions
+    // with the Slang library. At the moment, creating a session causes
+    // Slang to load and validate its standard library, so this is a
+    // somewhat heavy-weight operation. When possible, an application
+    // should try to re-use the same session across multiple compiles.
+    //
+    SlangSession* slangSession = spCreateSession(NULL);
+
+    // A compile request represents a single invocation of the compiler,
+    // to process some inputs and produce outputs (or errors).
+    //
+    SlangCompileRequest* slangRequest = spCreateCompileRequest(slangSession);
+
+    int targetIndex = spAddCodeGenTarget(slangRequest, SLANG_SPIRV);
+
+    // We will specify the desired "profile" for this one target in terms of the
+    // DirectX "shader model" that should be supported.
+    //
+   //spSetTargetProfile(slangRequest, targetIndex, spFindProfile(slangSession, "sm_4_0"));
+
+    // A compile request can include one or more "translation units," which more or
+    // less amount to individual source files (think `.c` files, not the `.h` files they
+    // might include).
+    //
+    // For this example, our code will all be in the Slang language. The user may
+    // also specify HLSL input here, but that currently doesn't affect the compiler's
+    // behavior much.
+    //
+    int translationUnitIndex = spAddTranslationUnit(slangRequest, SLANG_SOURCE_LANGUAGE_SLANG, nullptr);
+
+    // We will load source code for our translation unit from the file `shaders.slang`.
+    // There are also variations of this API for adding source code from application-provided buffers.
+    //
+    spAddTranslationUnitSourceFile(slangRequest, translationUnitIndex, "shaders.slang");
+
+    // Next we will specify the entry points we'd like to compile.
+    // It is often convenient to put more than one entry point in the same file,
+    // and the Slang API makes it convenient to use a single run of the compiler
+    // to compile all entry points.
+    //
+    // For each entry point, we need to specify the name of a function, the
+    // translation unit in which that function can be found, and the stage
+    // that we need to compile for (e.g., vertex, fragment, geometry, ...).
+    //
+    char const* vertexEntryPointName    = "vertexMain";
+    char const* fragmentEntryPointName  = "fragmentMain";
+    int vertexIndex   = spAddEntryPoint(slangRequest, translationUnitIndex, vertexEntryPointName,   SLANG_STAGE_VERTEX);
+    int fragmentIndex = spAddEntryPoint(slangRequest, translationUnitIndex, fragmentEntryPointName, SLANG_STAGE_FRAGMENT);
+
+    // Once all of the input options for the compiler have been specified,
+    // we can invoke `spCompile` to run the compiler and see if any errors
+    // were detected.
+    //
+    const SlangResult compileRes = spCompile(slangRequest);
+
+    // Even if there were no errors that forced compilation to fail, the
+    // compiler may have produced "diagnostic" output such as warnings.
+    // We will go ahead and print that output here.
+    //
+    if (auto diagnostics = spGetDiagnosticOutput(slangRequest))
+    {
+		std::cout << diagnostics << std::endl;
+    }
+
+    // If compilation failed, there is no point in continuing any further.
+    if (SLANG_FAILED(compileRes))
+    {
+        spDestroyCompileRequest(slangRequest);
+        spDestroySession(slangSession);
+
+		throw std::runtime_error("failed to compile slang shaders!");
+    }
+
+    // If compilation was successful, then we will extract the code for
+    // our two entry points as "blobs".
+    //
+    // If you are using a D3D API, then your application may want to
+    // take advantage of the fact taht these blobs are binary compatible
+    // with the `ID3DBlob`, `ID3D10Blob`, etc. interfaces.
+    //
+
+    ISlangBlob* vertexShaderBlob = nullptr;
+    spGetEntryPointCodeBlob(slangRequest, vertexIndex, 0, &vertexShaderBlob);
+
+    ISlangBlob* fragmentShaderBlob = nullptr;
+    spGetEntryPointCodeBlob(slangRequest, fragmentIndex, 0, &fragmentShaderBlob);
+
+    // We extract the begin/end pointers to the output code buffers
+    // using operations on the `ISlangBlob` interface.
+    //
+    char const* vertexCode = (char const*) vertexShaderBlob->getBufferPointer();
+    char const* vertexCodeEnd = vertexCode + vertexShaderBlob->getBufferSize();
+
+    char const* fragmentCode = (char const*) fragmentShaderBlob->getBufferPointer();
+    char const* fragmentCodeEnd = fragmentCode + fragmentShaderBlob->getBufferSize();
+
+    // Once we have extracted the output blobs, it is safe to destroy
+    // the compile request and even the session.
+    //
+    spDestroyCompileRequest(slangRequest);
+    spDestroySession(slangSession);
+
+    // Now we use the operations of the example graphics API abstraction
+    // layer to load shader code into the underlying API.
+    //
+    // Reminder: this section does not involve the Slang API at all.
+    //
+
+    // gfx::ShaderProgram::KernelDesc kernelDescs[] =
+    // {
+    //     { gfx::StageType::Vertex,    vertexCode,     vertexCodeEnd },
+    //     { gfx::StageType::Fragment,  fragmentCode,   fragmentCodeEnd },
+    // };
+
+    // gfx::ShaderProgram::Desc programDesc;
+    // programDesc.pipelineType = gfx::PipelineType::Graphics;
+    // programDesc.kernels = &kernelDescs[0];
+    // programDesc.kernelCount = 2;
+
+    // auto shaderProgram = renderer->createProgram(programDesc);
+
+	outVertexShaderData.insert(outVertexShaderData.begin(), vertexCode, vertexCodeEnd);
+	outFragmentShaderData.insert(outFragmentShaderData.begin(), fragmentCode, fragmentCodeEnd);
+
+    // Once we've used the output blobs from the Slang compiler to initialize
+    // the API-specific shader program, we can release their memory.
+    //
+    vertexShaderBlob->release();
+    fragmentShaderBlob->release();
+
+    //return shaderProgram;
+}
+
 
 template <typename T>
 constexpr auto sizeof_array(const T& array)
@@ -1413,7 +1551,12 @@ private:
 		ShaderModuleHashSet shaderModules;
 
 		std::vector<char> vsCode;
-		loadSPIRVFile("vert.spv", vsCode);
+		std::vector<char> fsCode;
+
+		//loadSPIRVFile("vert.spv", vsCode);
+		//loadSPIRVFile("frag.spv", fsCode);
+
+		loadSlangShaders(vsCode, fsCode);
 
 		VkShaderModuleCreateInfo vsCreateInfo = {};
 		vsCreateInfo.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
@@ -1429,9 +1572,6 @@ private:
 		vsStageInfo.stage = VK_SHADER_STAGE_VERTEX_BIT;
 		vsStageInfo.module = vsModule;
 		vsStageInfo.pName = "main";
-
-		std::vector<char> fsCode;
-		loadSPIRVFile("frag.spv", fsCode);
 
 		VkShaderModuleCreateInfo fsCreateInfo = {};
 		fsCreateInfo.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
