4d90528505efa7ffdf68777bd8757e68aeb1d7d2
Daniel Johansson <ainbusksingers@gmail.com>
2020-08-10 21:24:54 +0200

    SubmitInfo

--- examples/volcano/types.h
+++ examples/volcano/types.h
@@ -213,3 +213,6 @@ using CommandBufferLevel = std::conditional_t<B == Vk, VkCommandBufferLevel, std
 
 template <GraphicsBackend B>
 using RenderPassBeginInfo = std::conditional_t<B == Vk, VkRenderPassBeginInfo, std::nullptr_t>;
+
+template <GraphicsBackend B>
+using SubmitInfo = std::conditional_t<B == Vk, VkSubmitInfo, std::nullptr_t>;
