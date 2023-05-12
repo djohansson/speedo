e5c76b4804563d3742c2162ff439727e8da2833c
Daniel Johansson <1911831+djohansson@users.noreply.github.com>
2023-05-12 18:23:41 +0200

    prep for dynamic link of mimalloc

--- examples/volcano/application.cpp
+++ examples/volcano/application.cpp
@@ -1,9 +1,3 @@
 #include "application.h"
 
 #include <mimalloc-new-delete.h>
-
-struct MiMallocAllocator
-{
-	static inline void* malloc(std::size_t size) { return mi_malloc(size); }
-	static inline void free(void* ptr) { mi_free(ptr); }
-};
