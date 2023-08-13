124060694029bada85519fe75b09da01cd78be7c
Daniel Johansson <1911831+djohansson@users.noreply.github.com>
2023-08-13 19:50:41 +0200

    remove unused callbacks

--- src/rhi/vulkan/image.cpp
+++ src/rhi/vulkan/image.cpp
@@ -25,29 +25,6 @@
 
 #include <zpp_bits.h>
 
-namespace stbi_istream_callbacks
-{
-
-int read(void* user, char* data, int size)
-{
-	std::istream* stream = static_cast<std::istream*>(user);
-	return stream->rdbuf()->sgetn(data, size);
-}
-
-void skip(void* user, int size)
-{
-	std::istream* stream = static_cast<std::istream*>(user);
-	stream->seekg(size, std::ios::cur);
-}
-
-int eof(void* user)
-{
-	std::istream* stream = static_cast<std::istream*>(user);
-	return stream->tellg() != std::istream::pos_type(-1);
-}
-
-} // namespace stbi_istream_callbacks
-
 namespace image
 {
 
