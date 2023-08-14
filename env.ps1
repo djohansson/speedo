701848098c99571b4c083032d3c701f2bdbd02fc
Daniel Johansson <1911831+djohansson@users.noreply.github.com>
2023-08-14 17:14:09 +0200

    worked better development environment

--- env.ps1
+++ env.ps1
@@ -1,3 +1,47 @@
+function Get-Triplet
+{
+	$Arch = $Env:PROCESSOR_ARCHITECTURE
+	$Triplet = ""
+
+	if ($Arch -eq "AMD64")
+	{
+		$Triplet += "x64-"
+	}
+	# TODO: Add support for other architectures
+	# elseif ($Arch -eq "x86")
+	# {
+	# 	$Triplet += "x86-"
+	# }
+	# elseif ($Arch -eq "ARM64")
+	# {
+	# 	$Triplet += "arm64-"
+	# }
+	else
+	{
+		Write-Error "Unsupported architecture: $Arch"
+	}
+
+	if ($IsWindows)
+	{
+		$Triplet += "windows-clang"
+	}
+	# TODO: Add support for other operating systems
+	# elseif ($IsMacOS)
+	# {
+	# 	$Triplet += "osx-clang"
+	# }
+	# elseif ($IsLinux)
+	# {
+	# 	$Triplet += "linux-clang"
+	# }
+	else
+	{
+		Write-Error "Unsupported Operating System"
+	}
+
+	return $Triplet
+}
+
 function Read-EnvFile
 {
 	param([Parameter(Mandatory = $True, Position = 0)] [string] $envFile)
@@ -6,28 +50,45 @@ function Read-EnvFile
 
 	foreach( $property in $localEnv.psobject.properties)
 	{
-		Write-Host $property.Name "=" $property.Value
 		[Environment]::SetEnvironmentVariable($property.Name, $property.Value)
+
+		#Write-Host $property.Name "=" $property.Value
 	}
 }
 
 function Initialize-DevEnv
 {
-	Write-Host "Adding installed vcpkg packages to env:Path..."
+	#Write-Host "Adding installed vcpkg packages to env:Path..."
 
-	$BinDirectory = "$PSScriptRoot\build.vcpkg-installed\$Triplet\bin"
+	$BinDirectory = "$PSScriptRoot\build.vcpkg-installed\$triplet\bin"
 	$env:Path += ";$BinDirectory"
 
-	Write-Host $BinDirectory
+	#Write-Host $BinDirectory
 
-	foreach($ToolsDirectory in Get-ChildItem -Path $PSScriptRoot\build.vcpkg-installed\$Triplet\tools -Directory)
+	$triplet = Get-Triplet
+
+	foreach($ToolsDirectory in Get-ChildItem -Path $PSScriptRoot\build.vcpkg-installed\$triplet\tools -Directory)
 	{
 		$env:Path += ";$ToolsDirectory"
 
-		Write-Host $ToolsDirectory
+		#Write-Host $ToolsDirectory
 	}
 	
-	Write-Host "Setting environment variables..."
+	#Write-Host "Setting environment variables..."
 
 	Read-EnvFile "$PSScriptRoot/env.json"
+}
+
+function Invoke-DevEnv
+{
+	param(
+		[Parameter(Mandatory = $True, Position = 0)] [string] $command,
+		[Parameter(Mandatory = $True, ValueFromRemainingArguments = $true, Position = 1)][string[]] $argsList
+	)
+
+	Initialize-DevEnv
+
+	$arguments = Join-String -Separator " " -InputObject $argsList
+
+	Invoke-Expression ($command + " " + $arguments)
 }
--- setup.ps1
+++ setup.ps1
@@ -1,57 +1,16 @@
-$Arch = $Env:PROCESSOR_ARCHITECTURE
-$Triplet = ""
-
-if ($Arch -eq "AMD64")
-{
-	$Triplet += "x64-"
-}
-# TODO: Add support for other architectures
-# elseif ($Arch -eq "x86")
-# {
-# 	$Triplet += "x86-"
-# }
-# elseif ($Arch -eq "ARM64")
-# {
-# 	$Triplet += "arm64-"
-# }
-else
-{
-	Write-Error "Unsupported architecture: $Arch"
-	
-	return
-}
-
-if ($IsWindows)
-{
-	$Triplet += "windows-clang"
-}
-# TODO: Add support for other operating systems
-# elseif ($IsMacOS)
-# {
-# 	$Triplet += "osx"
-# }
-# elseif ($IsLinux)
-# {
-# 	$Triplet += "linux"
-# }
-else
-{
-	Write-Error "Unsupported Operating System"
-	
-	return
-}
-
 . $PSScriptRoot/env.ps1
 
 $myEnvFile = "$PSScriptRoot/env.json"
 $myEnv = New-Object -TypeName PSObject
+$myTriplet = Get-Triplet
 
 if (Test-Path $myEnvFile)
 {
 	$myEnv = Get-Content -Path $myEnvFile -Raw | ConvertFrom-Json
 }
 
-# TODO: use targeted versions of all these winget packages.
+Write-Host "Installing dependencies..."
+
 if ($IsWindows)
 {
 	if (-not (Get-InstalledModule Microsoft.WinGet.Client -ErrorAction SilentlyContinue))
@@ -67,7 +26,7 @@ if ($IsWindows)
 	{ 
 		Write-Host "Installing Powershell Core..."
 
-		Install-WinGetPackage -Mode Silent -Id Microsoft.PowerShell
+		Install-WinGetPackage -Mode Silent -Id Microsoft.PowerShell | Out-Null
 
 		$pwshCmd = Get-Command "pwsh" -All -ErrorAction SilentlyContinue | Where-Object Version -GE ([System.Version]"7.0.0.0")
 	}
@@ -79,32 +38,39 @@ if ($IsWindows)
 	{
 		Write-Host "Installing Git..."
 
-		Install-WinGetPackage -Mode Silent -Id Git.Git
+		Install-WinGetPackage -Mode Silent -Id Git.Git | Out-Null
 
 		$gitCmd = Get-Command "git" -All -ErrorAction SilentlyContinue | Where-Object Version -GE ([System.Version]"2.41.0.0")
 	}
 	$myEnv | Add-Member -Force -PassThru -NotePropertyName GIT_PATH -NotePropertyValue (Split-Path -Path $gitCmd.Source) | Out-Null
 
 	$vulkanSdkInfo = Get-WinGetPackage KhronosGroup.VulkanSDK
+	$vulkanSdkInfoHashTable = @{}
+	$vulkanSdkInfo.psobject.properties | ForEach-Object { $vulkanSdkInfoHashTable[$_.Name] = $_.Value }
 
 	if (-not ($vulkanSdkInfo) -or ($vulkanSdkInfo.InstalledVersion -lt ([System.Version]"1.3.231.1")))
 	{
 		Write-Host "Installing VulkanSDK (requires process elevation)..."
 
-		Start-Process pwsh -Verb runas -ArgumentList "-c Install-WinGetPackage -Mode Silent -Id KhronosGroup.VulkanSDK"
+		Start-Process pwsh -Verb runas -ArgumentList "-c Install-WinGetPackage -Mode Silent -Id KhronosGroup.VulkanSDK | Out-Null"
 	}
 	$myEnv | Add-Member -Force -PassThru -NotePropertyName VULKAN_SDK -NotePropertyValue "C:\VulkanSDK\1.3.231.1" | Out-Null
+	$myEnv | Add-Member -Force -PassThru -NotePropertyName VULKAN_SDK_VERSION -NotePropertyValue $vulkanSdkInfoHashTable["InstalledVersion"] | Out-Null
 
 	$windowsSdkInfo = Get-WinGetPackage Microsoft.WindowsSDK.10.0.22621
+	$windowsSdkInfoHashTable = @{}
+	$windowsSdkInfo.psobject.properties | ForEach-Object { $windowsSdkInfoHashTable[$_.Name] = $_.Value }
+	$windowsSdkVersion = $windowsSdkInfoHashTable["InstalledVersion"]
+	$windowsSdkVersionShort = $windowsSdkVersion.SubString(0, $windowsSdkVersion.LastIndexOf('.')) + ".0" #workaround for paths not matching the full version number
 
 	if (-not ($windowsSdkInfo))
 	{
 		Write-Host "Installing WindowsSDK 10.0.22621 (requires process elevation)..."
 
-		Start-Process pwsh -Verb runas -ArgumentList "-c Install-WinGetPackage -Mode Silent -Id Microsoft.WindowsSDK.10.0.22621"
+		Start-Process pwsh -Verb runas -ArgumentList "-c Install-WinGetPackage -Mode Silent -Id Microsoft.WindowsSDK.10.0.22621 | Out-Null"
 	}
 	$myEnv | Add-Member -Force -PassThru -NotePropertyName WINDOWS_SDK -NotePropertyValue "C:\Program Files (x86)\Windows Kits\10" | Out-Null
-	$myEnv | Add-Member -Force -PassThru -NotePropertyName WINDOWS_SDK_VERSION -NotePropertyValue "10.0.22621.0" | Out-Null
+	$myEnv | Add-Member -Force -PassThru -NotePropertyName WINDOWS_SDK_VERSION -NotePropertyValue $windowsSdkVersionShort | Out-Null
 
 	if (-not (Get-InstalledModule VSSetup -ErrorAction SilentlyContinue))
 	{
@@ -113,20 +79,34 @@ if ($IsWindows)
 		Install-Module VSSetup -Scope CurrentUser -Confirm:$False -Force
 	}
 
-	$VSSetupInstance = Get-VSSetupInstance | Select-VSSetupInstance -Product * -Require "Microsoft.VisualStudio.Workload.VCTools","Microsoft.VisualStudio.Component.VC.Llvm.Clang","Microsoft.VisualStudio.Component.VC.Llvm.ClangToolset"
+	$VSSetupInstance = Get-VSSetupInstance | Select-VSSetupInstance -Product * -Require "Microsoft.VisualStudio.Workload.VCTools"
 
 	if (-not ($VSSetupInstance))
 	{
 		Write-Host "Installing VisualStudio 2022 VC BuildTools..."
 
 		# --force is required to circumvent the fact that Microsoft.VisualStudio.2022.BuildTools could already be installed without the VCTools workload
-		winget install -e -h --id Microsoft.VisualStudio.2022.BuildTools --override "--quiet --add Microsoft.VisualStudio.Workload.VCTools;includeRecommended --add Microsoft.VisualStudio.Component.VC.Llvm.Clang --add Microsoft.VisualStudio.Component.VC.Llvm.ClangToolset --wait" --force
+		winget install -e -h --id Microsoft.VisualStudio.2022.BuildTools --override "--quiet --add Microsoft.VisualStudio.Workload.VCTools;includeRecommended --wait" --force
 
-		$VSSetupInstance = Get-VSSetupInstance | Select-VSSetupInstance -Product * -Require "Microsoft.VisualStudio.Workload.VCTools","Microsoft.VisualStudio.Component.VC.Llvm.Clang","Microsoft.VisualStudio.Component.VC.Llvm.ClangToolset"
+		$VSSetupInstance = Get-VSSetupInstance | Select-VSSetupInstance -Product * -Require "Microsoft.VisualStudio.Workload.VCTools"
 	}
 	$myEnv | Add-Member -Force -PassThru -NotePropertyName VISUAL_STUDIO_PATH -NotePropertyValue $VSSetupInstance.InstallationPath | Out-Null
-	$myEnv | Add-Member -Force -PassThru -NotePropertyName VISUAL_STUDIO_TOOLCHAIN_VERSION -NotePropertyValue "14.37.32822" | Out-Null
-	$myEnv | Add-Member -Force -PassThru -NotePropertyName LLVM_PATH -NotePropertyValue ($VSSetupInstance.InstallationPath + "\VC\Tools\Llvm") | Out-Null
+	$myEnv | Add-Member -Force -PassThru -NotePropertyName VISUAL_STUDIO_VCTOOLS_VERSION -NotePropertyValue (Get-Content -Path ($VSSetupInstance.InstallationPath + "\VC\Auxiliary\Build\Microsoft.VCToolsVersion.default.txt")) | Out-Null
+
+	$llvmInfo = Get-WinGetPackage LLVM.LLVM
+	$llvmInfoHashTable = @{}
+	$llvmInfo.psobject.properties | ForEach-Object { $llvmInfoHashTable[$_.Name] = $_.Value }
+	$llvmVersion = $llvmPkgHashTable["InstalledVersion"]
+	$llvmVersionShort = $llvmVersion.Substring(0, $llvmVersion.IndexOf('.')) #workaround for paths not matching the full version number
+
+	if (-not ($llvmInfo) -or ($llvmInfo.InstalledVersion -lt ([System.Version]"16.0.6"))) # todo: set this to 17.0.0 once it's available
+	{
+		Write-Host "Installing LLVM (requires process elevation)..."
+
+		Install-WinGetPackage -Mode Silent -Id LLVM.LLVM | Out-Null
+	}
+	$myEnv | Add-Member -Force -PassThru -NotePropertyName LLVM_PATH -NotePropertyValue "C:\Program Files\LLVM" | Out-Null
+	$myEnv | Add-Member -Force -PassThru -NotePropertyName LLVM_VERSION -NotePropertyValue $llvmVersionShort | Out-Null
 }
 
 $myEnv | ConvertTo-Json | Out-File $myEnvFile
@@ -144,8 +124,8 @@ else
 	Invoke-Expression(Invoke-WebRequest -useb https://aka.ms/vcpkg-init.ps1)
 }
 
-Write-Host "Installing vcpkg packages for $Triplet using manifest..."
+Write-Host "Installing vcpkg packages for $myTriplet using manifest..."
 
-vcpkg install --x-install-root="build.vcpkg-installed" --overlay-triplets=$PSScriptRoot --triplet $Triplet --x-feature=client --x-feature=server --x-feature=tests --no-print-usage
+vcpkg install --x-install-root="build.vcpkg-installed" --overlay-triplets=$PSScriptRoot --triplet $myTriplet --x-feature=client --x-feature=server --x-feature=tests --no-print-usage
 
 Initialize-DevEnv
