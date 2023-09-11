de83e64e75d6fc67da89ad817289c750f8eb664a
Daniel Johansson <1911831+djohansson@users.noreply.github.com>
2023-09-11 09:19:27 +0200

    update backlog

--- src/rhi/application.h
+++ src/rhi/application.h
@@ -15,13 +15,12 @@
 // todo: untangle client dependencies
 // todo: graph based GUI. current solution (imnodes) is buggy and not currently working at all.
 // todo: what if the thread pool could monitor Host+Device visible memory heap using atomic_wait? then we could trigger callbacks on GPU completion events with minimum latency.
+// todo: remove all use of preprocessor macros, and replace with constexpr functions so that we can migrate to using modules.
 
-// in progress: replace all external deps with vcpkg packages
+// in progress: clean up utils.h and split it into multiple files. a lot of the things in there can likely be removed once support emerges in std (flat containers etc)
 // in progress: streamlined project setup process on all platforms.
-// in progress: replace cereal with glaze & zpp::bits
 
-// done: separate IMGUI and client abstractions more clearly. avoid referencing IMGUI:s windowdata
-// 		 members where possible
+// done: separate IMGUI and client abstractions more clearly. avoid referencing IMGUI:s windowdata members where possible
 // done: instrumentation and timing information
 // done: organize secondary command buffers into some sort of pool, and schedule them on a couple of worker threads
 // done: move stuff from headers into compilation units
@@ -30,6 +29,8 @@
 // done: port slang into vcpkg package
 // done: move volcano into own github repo and rename to something else (speedo)
 // done: migrate out of slang into own root and clean up folder structure
+// done: replace all external deps with vcpkg packages
+// done: replace cereal with glaze & zpp::bits
 
 // cut: dynamic mesh layout, depending on input data structure. (use GLTF instead)
 
