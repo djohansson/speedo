58017a608ec1d046e4b24de69cd24a8cf12e10d2
Daniel Johansson <1911831+djohansson@users.noreply.github.com>
2023-11-06 23:34:16 +0100

    cleanup

--- src/core/mio_extra.h
+++ src/core/mio_extra.h
@@ -4,18 +4,20 @@
 
 namespace mio_extra
 {
-	struct resizeable_mmap_sink : public mio::mmap_sink
-	{
-		resizeable_mmap_sink(const std::string& path, const size_type offset = 0, const size_type length = mio::map_entire_file)
-			: mio::mmap_sink(path, offset, length)
-		{}
 
-		void resize(size_t size)
-		{
-			std::error_code error;
-			mio::mmap_sink::remap(0, size, error);
-			if (error)
-				throw std::system_error(std::move(error));
-		}
-	};
-}
+struct resizeable_mmap_sink : public mio::mmap_sink
+{
+	resizeable_mmap_sink(const std::string& path, const size_type offset = 0, const size_type length = mio::map_entire_file)
+		: mio::mmap_sink(path, offset, length)
+	{}
+
+	void resize(size_t size)
+	{
+		std::error_code error;
+		mio::mmap_sink::remap(0, size, error);
+		if (error)
+			throw std::system_error(std::move(error));
+	}
+};
+
+} // namespace mio_extra
