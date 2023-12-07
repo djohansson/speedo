2428966eaf0b71a3651d2f92799a0c9660d4b4bd
Daniel Johansson <1911831+djohansson@users.noreply.github.com>
2023-12-07 20:20:44 +0100

    plain .UTF-8 does not work on mac apparently

--- src/core/application.cpp
+++ src/core/application.cpp
@@ -12,6 +12,6 @@ Application::Application(std::string_view name, Environment&& env)
 {
 	assertf(theApplication.use_count() == 0, "There can only be one application at a time");
 	
-	std::locale utf8Locale(".UTF8");  	
+	std::locale utf8Locale("en_US.UTF-8");  	
 	std::locale::global(utf8Locale);
 }
