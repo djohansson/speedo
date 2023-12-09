3250e59d03b07d1ea1c7d6824b20003c0659ca58
Daniel Johansson <1911831+djohansson@users.noreply.github.com>
2023-12-09 22:48:42 +0100

    prevent mimalloc from being thrown out by the linker

--- tests/core/core.cpp
+++ tests/core/core.cpp
@@ -1,6 +1,10 @@
 #include <catch2/catch_session.hpp>
 
+#include <mimalloc.h>
+
 int main(int argc, char* argv[])
 {
+	printf("mi_version(): %d\n", mi_version());
+
 	return Catch::Session().run( argc, argv );
 }
