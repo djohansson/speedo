eabd51bba3f10ec99703ef0c23ca49bc61032b8f
Daniel Johansson <1911831+djohansson@users.noreply.github.com>
2023-12-10 15:37:42 +0100

    add patchelf package + prep for vulkan sdk install

--- setup_macos.sh
+++ setup_macos.sh
@@ -23,11 +23,4 @@ brew_install_package() {
 brew_install
 brew_install_package "powershell/tap/powershell"
 
-#curl -O https://sdk.lunarg.com/sdk/download/latest/mac/vulkan_sdk.dmg
-#hdiutil attach vulkan_sdk.dmg
-#sudo ./InstallVulkan.app/Contents/MacOS/InstallVulkan --root $(mkdir build.vulkan; cd ./build.vulkan ; pwd) --accept-licenses --default-answer --confirm-command install
-#hdiutil detach /Volumes/vulkansdk-macos-*
-#rm vulkan_sdk.dmg
-#sudo python3 ~/VulkanSDK/*/install_vulkan.py --install-json-location ~/VulkanSDK/*/
-
 pwsh -f ./setup.ps1
