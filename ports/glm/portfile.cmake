43f3dd9f48646964d26bf132e8511bc7886e1820
Daniel Johansson <1911831+djohansson@users.noreply.github.com>
2024-02-19 16:34:04 +0100

    remove unused directory

--- ports/glm/portfile.cmake
+++ ports/glm/portfile.cmake
@@ -20,7 +20,7 @@ vcpkg_cmake_configure(
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup()
 
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/copying.txt")
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
