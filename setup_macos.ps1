7df68d430055bc19c97686ec2bda8a255d934749
Daniel Johansson <1911831+djohansson@users.noreply.github.com>
2024-03-11 22:21:14 +0100

    add molten vk dependency and remove python on mac

--- setup_macos.ps1
+++ setup_macos.ps1
@@ -3,19 +3,17 @@
 Write-Host "Installing MacOS dependencies..."
 
 $(xcode-select --install) 2>&1 | Out-Null
-$global:myEnv | Add-Member -Force -PassThru -NotePropertyName MACOS_SDK_PATH -NotePropertyValue $(xcrun --show-sdk-path) | Out-Null
-
-$pwshPath = $(brew --prefix powershell)
-$global:myEnv | Add-Member -Force -PassThru -NotePropertyName POWERSHELL_PATH -NotePropertyValue $pwshPath/bin | Out-Null
 
 Install-HomebrewPackage pkg-config
 Install-HomebrewPackage libxinerama
 Install-HomebrewPackage libxcursor
 Install-HomebrewPackage mesa-glu
-Install-HomebrewPackage python
 Install-HomebrewPackage patchelf
-
 Install-HomebrewPackage llvm
-$llvmPath = $(brew --prefix llvm)
-$global:myEnv | Add-Member -Force -PassThru -NotePropertyName LLVM_PATH -NotePropertyValue $llvmPath | Out-Null
+Install-HomebrewPackage molten-vk
+
+$global:myEnv | Add-Member -Force -PassThru -NotePropertyName MACOS_SDK_PATH -NotePropertyValue $(xcrun --show-sdk-path) | Out-Null
+$global:myEnv | Add-Member -Force -PassThru -NotePropertyName POWERSHELL_PATH -NotePropertyValue $($(brew --prefix powershell) + '/bin') | Out-Null
+$global:myEnv | Add-Member -Force -PassThru -NotePropertyName LLVM_PATH -NotePropertyValue $(brew --prefix llvm) | Out-Null
 $global:myEnv | Add-Member -Force -PassThru -NotePropertyName LLVM_VERSION -NotePropertyValue $(brew info llvm | grep "llvm:" | egrep -o '(\d+\.\d+\.\d+-?\w*)') | Out-Null
+$global:myEnv | Add-Member -Force -PassThru -NotePropertyName MOLTEN_VK_PATH -NotePropertyValue $(brew --prefix molten-vk) | Out-Null
