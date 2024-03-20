f2d8cac93bcb58144ed23313f78a487128b8bb62
Daniel Johansson <1911831+djohansson@users.noreply.github.com>
2024-03-20 23:28:33 +0100

    more correct use of atomics

--- src/core/circularcontainer.h
+++ src/core/circularcontainer.h
@@ -8,15 +8,15 @@ template <typename T, typename ContainerT = std::vector<T>>
 class CircularContainer : public ContainerT
 {
 public:
-	T& get() { return ContainerT::at(myHead); }
-	const T& get() const { return ContainerT::at(myHead); }
+	T& get() { return ContainerT::at(myHead.load(std::memory_order_relaxed)); }
+	const T& get() const { return ContainerT::at(myHead.load(std::memory_order_relaxed)); }
 
 	T& fetchAdd(size_t offset = 1u)
 	{
 		size_t index = myHead.fetch_add(offset, std::memory_order_relaxed);
-		size_t newHeadWithOverflow = myHead;
+		size_t newHeadWithOverflow = index + offset;
 		size_t newHeadModulo = newHeadWithOverflow % ContainerT::size();
-		while (!myHead.compare_exchange_weak(newHeadWithOverflow, newHeadModulo, std::memory_order_relaxed))
+		while (!myHead.compare_exchange_weak(newHeadWithOverflow, newHeadModulo, std::memory_order_acq_rel))
 			newHeadModulo = newHeadWithOverflow % ContainerT::size();
 		
 		return ContainerT::at(index);
