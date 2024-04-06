c427b85cc888f114e9fa1c329052e6a9bd44b1b7
Daniel Johansson <1911831+djohansson@users.noreply.github.com>
2024-04-06 17:55:08 +0200

    signed oct encode/decode

--- src/rhi/shaders/capi.h
+++ src/rhi/shaders/capi.h
@@ -6,7 +6,8 @@ extern "C"
 #endif
 
 #if defined(SHADERTYPES_H_CPU_TARGET)
-#	include <cstdint>
+#	include <stdint.h>
+#	include <limits.h>
 #	include <glm/glm.hpp>
 #	define float4x4 glm::mat4x4
 #	define float4 glm::vec4
@@ -15,6 +16,10 @@ extern "C"
 #	define uint uint32_t
 #else
 #	define alignas(x)
+#	define FLT_MAX 3.402823466e+38
+#	define FLT_MIN 1.175494351e-38
+#	define DBL_MAX 1.7976931348623158e+308
+#	define DBL_MIN 2.2250738585072014e-308
 #endif
 
 #define DescriptorSetCategory_Global 0
