05715f46285e7cbc0f7daa45254db0840ed1fe47
Daniel Johansson <1911831+djohansson@users.noreply.github.com>
2024-05-15 09:09:41 +0200

    nitpicking

--- src/core/file.cpp
+++ src/core/file.cpp
@@ -29,10 +29,10 @@ std::string GetTimeStamp(const std::filesystem::path& filePath)
 {
 	ZoneScoped;
 
-	auto sec = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::duration_cast<std::chrono::milliseconds>(
-		std::filesystem::last_write_time(filePath).time_since_epoch()));
-	std::time_t timestamp = sec.count();
-	static constexpr auto kBufferSize = 80;
+	std::time_t timestamp = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::duration_cast<std::chrono::milliseconds>(
+		std::filesystem::last_write_time(filePath).time_since_epoch())).count();
+	
+	static constexpr size_t kBufferSize = 80;
 	std::array<char, kBufferSize> buffer;
 
 	return {buffer.data(), std::strftime(buffer.data(), buffer.size(), "%c", std::localtime(&timestamp))};
