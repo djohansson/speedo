20a30c664874d87767bdf262d9cc5024a3c2427d
Daniel Johansson <1911831+djohansson@users.noreply.github.com>
2024-05-22 21:42:34 +0200

    added terminate handler

--- src/core/application.cpp
+++ src/core/application.cpp
@@ -1,5 +1,8 @@
 #include "application.h"
 
+#include <exception>
+#include <iostream>
+
 #include <mimalloc-new-delete.h>
 
 std::weak_ptr<Application> gApplication;
@@ -10,6 +13,30 @@ Application::Application(std::string_view name, Environment&& env)
 , myExecutor(std::make_unique<TaskExecutor>(std::max(1, static_cast<int>(std::thread::hardware_concurrency()) - 3)))
 {
 	ASSERTF(gApplication.use_count() == 1, "There can only be one application at a time");
+	std::set_terminate([]()
+	{
+		try
+		{
+			std::exception_ptr eptr{std::current_exception()};
+			if (eptr)
+			{
+				std::rethrow_exception(eptr);
+			}
+			else
+			{
+				std::cerr << "Exiting without exception\n";
+			}
+		}
+		catch (const std::exception& exc)
+		{
+			std::cerr << "Exception: " << exc.what() << '\n';
+		}
+		catch (...)
+		{
+			std::cerr << "Unknown exception caught\n";
+		}
+		std::exit(EXIT_FAILURE);
+	});
 }
 
 void Application::OnMouse(const MouseEvent& mouse)
