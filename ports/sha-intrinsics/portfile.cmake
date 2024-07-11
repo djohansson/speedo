1ca88f40aa8ba8046564c52219cb0612ca327f92
Daniel Johansson <1911831+djohansson@users.noreply.github.com>
2024-07-11 23:00:46 +0200

    bumped ref

--- ports/sha-intrinsics/portfile.cmake
+++ ports/sha-intrinsics/portfile.cmake
@@ -2,8 +2,8 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO djohansson/SHA-Intrinsics
-    REF 22798a18203f18e17eb0f3a1cba2cbbdabe5d618
-    SHA512 82f7f1de0d1dc5d70a00b19210d9f8c5dcb77888ad504663ecf6fbed6285b8b9fa29a40d6d87ee93c9a4ebb0970a7a66b10b5ff46ba40c03152ffd4e6fdc3f4f
+    REF 1da6cf076c6b062f67db1c834b7488b9ef269587
+    SHA512 5ed68cf080bcc0ec4096465ab3a2a20b8a0681a20df45d14e855739cb0c73c0ff7efb0cf02afaea08f68e05a050370738809bf48bf048a627b5b15436253edc9
     HEAD_REF master
 )
 vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}")
