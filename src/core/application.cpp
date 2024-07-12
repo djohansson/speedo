ce9eeaf41a17a0c3433256ddd8c1bf7cc4d5977d
Daniel Johansson <1911831+djohansson@users.noreply.github.com>
2024-07-13 00:06:35 +0200

    tweak thread number from ncores-3 to ncores-2 to allow for two task pool threads on quadcore archs

--- src/core/application.cpp
+++ src/core/application.cpp
@@ -9,7 +9,7 @@ std::weak_ptr<Application> gApplication;
 Application::Application(std::string_view name, Environment&& env)
 : myName(name)
 , myEnvironment(std::forward<Environment>(env))
-, myExecutor(std::make_unique<TaskExecutor>(std::max(1, static_cast<int>(std::thread::hardware_concurrency()) - 3)))
+, myExecutor(std::make_unique<TaskExecutor>(std::max(1, static_cast<int>(std::thread::hardware_concurrency()) - 2)))
 {
 	ASSERTF(gApplication.use_count() == 1, "There can only be one application at a time");
 	std::set_terminate([]()
