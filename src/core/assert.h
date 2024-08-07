db47ba10a5f4b1b1165cf10d6f1711e6b9787d96
Daniel Johansson <1911831+djohansson@users.noreply.github.com>
2024-08-07 10:59:35 +0200

    use std::println instead of fprintf in c++

--- src/core/assert.h
+++ src/core/assert.h
@@ -1,8 +1,15 @@
 #pragma once
 
-#include <errno.h>
-#include <stdio.h>
-#include <string.h>
+#ifdef __cplusplus
+#	include <cerrno>
+#	include <cstdio>
+#	include <cstring>
+#	include <print>
+#else
+#	include <errno.h>
+#	include <stdio.h>
+#	include <string.h>
+#endif
 
 #ifndef __has_builtin
 #	define __has_builtin(x) 0
@@ -14,7 +21,12 @@
 #endif
 
 #define CLEAN_ERRNO() (errno == 0 ? "NULL" : strerror(errno))
-#define LOG_ERROR(M, ...) fprintf(stderr, "[ERROR] (%s:%d: errno: %s) " M "\n", __FILE__, __LINE__, CLEAN_ERRNO(), ##__VA_ARGS__) //NOLINT(modernize-use-std-print)
+
+#ifdef __cplusplus
+#define LOG_ERROR(M, ...) std::println(stderr, "[ERROR] ({}:{}: errno: {}) " M, __FILE__, __LINE__, CLEAN_ERRNO(), ##__VA_ARGS__)
+#else
+#define LOG_ERROR(M, ...) fprintf(stderr, "[ERROR] (%s:%d: errno: %s) " M "\n", __FILE__, __LINE__, CLEAN_ERRNO(), ##__VA_ARGS__)
+#endif
 
 #ifdef NDEBUG
 #	define ASSERT(A)
