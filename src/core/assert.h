dd424e64dd7bab4656c16243b3defd5e956e08e5
Daniel Johansson <1911831+djohansson@users.noreply.github.com>
2024-08-25 23:57:17 +0200

    dont use cpp style printf:s just yet

--- src/core/assert.h
+++ src/core/assert.h
@@ -23,56 +23,57 @@
 
 #define CLEAN_ERRNO() (errno == 0 ? "NULL" : strerror(errno))
 
-#ifdef __cplusplus
-#	define LOG_ERROR(M, ...) std::println(stderr, "{}:{}\nerrno: {}\n{}\n" M, __FILE__, __LINE__, CLEAN_ERRNO(), cpptrace::generate_trace().to_string(), ##__VA_ARGS__);
-#else
-#	define LOG_ERROR(M, ...) fprintf(stderr, "%s:%d\nerrno: %s\n" M, __FILE__, __LINE__, CLEAN_ERRNO(), ##__VA_ARGS__);
-#endif
+// #ifdef __cplusplus
+// //#	define LOG_ERROR(M, ...) std::println(stderr, "{}:{} (errno: {})\n{}\n" M "\n", __FILE__, __LINE__, CLEAN_ERRNO(), cpptrace::generate_trace().to_string(), __VA_ARGS__)
+// #	define LOG_ERROR(M, ...) std::println(stderr, "{}:{} (errno: {})\n" M "\n", __FILE__, __LINE__, CLEAN_ERRNO(), __VA_ARGS__)
+// #else
+#	define LOG_ERROR(M, ...) fprintf(stderr, "%s:%d (errno: %s)\n" M "\n", __FILE__, __LINE__, CLEAN_ERRNO(), __VA_ARGS__)
+//#endif
 
 #ifdef NDEBUG
 #	define ASSERT(A) ((void)(A))
 #	define ASSERTF(A, M, ...) ((void)(A))
 #else
-#ifdef __cplusplus
-#	define ASSERT(A)                                                                               \
-		if (!(A))                                                                                  \
-		{                                                                                          \
-			LOG_ERROR("Assertion failed: {}", #A);                                                 \
-			TRAP();                                                                                \
-		}
-#else
+// #ifdef __cplusplus
+// #	define ASSERT(A)                                                                               \
+// 		if (!(A))                                                                                  \
+// 		{                                                                                          \
+// 			LOG_ERROR("Assertion failed: {}", #A);                                                 \
+// 			TRAP();                                                                                \
+// 		}
+// #else
 #	define ASSERT(A)                                                                               \
 		if (!(A))                                                                                  \
 		{                                                                                          \
 			LOG_ERROR("Assertion failed: %s", #A);                                                 \
 			TRAP();                                                                                \
 		}
-#endif
+//#endif
 #	define ASSERTF(A, M, ...)                                                                      \
 		if (!(A))                                                                                  \
 		{                                                                                          \
-			LOG_ERROR(M, ##__VA_ARGS__);                                                           \
+			LOG_ERROR(M, __VA_ARGS__);                                                             \
 			TRAP();                                                                                \
 		}
 #endif
-#ifdef __cplusplus
-#	define CHECK(A)                                                                                \
-		if (!(A))                                                                                  \
-		{                                                                                          \
-			LOG_ERROR("Check failed: {}", #A);                                                     \
-			TRAP();                                                                                \
-		}
-#else
+// #ifdef __cplusplus
+// #	define CHECK(A)                                                                                \
+// 		if (!(A))                                                                                  \
+// 		{                                                                                          \
+// 			LOG_ERROR("Check failed: {}", #A);                                                     \
+// 			TRAP();                                                                                \
+// 		}
+// #else
 #	define CHECK(A)                                                                                \
 		if (!(A))                                                                                  \
 		{                                                                                          \
 			LOG_ERROR("Check failed: %s", #A);                                                     \
 			TRAP();                                                                                \
 		}
-#endif
+//#endif
 #define CHECKF(A, M, ...)                                                                          \
 	if (!(A))                                                                                      \
 	{                                                                                              \
-		LOG_ERROR(M, ##__VA_ARGS__);                                                               \
+		LOG_ERROR(M, __VA_ARGS__);                                                                 \
 		TRAP();                                                                                    \
 	}
