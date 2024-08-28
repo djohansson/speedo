d8f0d0af1f002f352af9d8b404b52ef61568a1bb
Daniel Johansson <1911831+djohansson@users.noreply.github.com>
2024-08-28 18:03:35 +0200

    remove parameter quotes

--- run.ps1
+++ run.ps1
@@ -8,5 +8,5 @@ $triplet = Get-NativeTriplet
 Add-EnvPath $PSScriptRoot/build.vcpkg/$triplet/bin
 Add-EnvDylibPath $PSScriptRoot/build.vcpkg/$triplet/lib
 
-& $PSScriptRoot/build.output/$config/client -u '$HOME/.speedo' &
-& $PSScriptRoot/build.output/$config/server -u '$HOME/.speedo'
+& $PSScriptRoot/build.output/$config/client -u $HOME/.speedo &
+& $PSScriptRoot/build.output/$config/server -u $HOME/.speedo
