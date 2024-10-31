880c4dbd84076f44c3012f25a2c1f057259f55a4
Daniel Johansson <1911831+djohansson@users.noreply.github.com>
2024-10-31 14:34:46 +0100

    add version.h

---
+++ src/version.h
@@ -0,0 +1,24 @@
+#pragma once
+
+namespace speedo
+{
+
+namespace version
+{
+
+enum
+{
+	Major = 0
+};
+enum
+{
+	Minor = 1
+};
+enum
+{
+	Patch = 0
+};
+
+} // namespace version
+
+} // namespace speedo
