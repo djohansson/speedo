d13384a80404bb7e52b5683459e43c445c1061cf
Daniel Johansson <1911831+djohansson@users.noreply.github.com>
2025-04-03 19:36:57 +0200

    set color target flags to allow copy & compute

--- src/rhi/vulkan/rhi.cpp
+++ src/rhi/vulkan/rhi.cpp
@@ -309,7 +309,7 @@ void ConstructWindowDependentObjects(RHI<kVk>& rhi)
 				.mipLevels = {{.extent = window.GetConfig().swapchainConfig.extent}},
 				.format = window.GetConfig().swapchainConfig.surfaceFormat.format,
 				.tiling = VK_IMAGE_TILING_OPTIMAL,
-				.usageFlags = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT/* | VK_IMAGE_USAGE_TRANSFER_SRC_BIT*/ | VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_SAMPLED_BIT | VK_IMAGE_USAGE_STORAGE_BIT,
+				.usageFlags = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT | VK_IMAGE_USAGE_TRANSFER_SRC_BIT/* | VK_IMAGE_USAGE_TRANSFER_DST_BIT*/ | VK_IMAGE_USAGE_SAMPLED_BIT | VK_IMAGE_USAGE_STORAGE_BIT,
 				.memoryFlags = VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
 				.imageAspectFlags = VK_IMAGE_ASPECT_COLOR_BIT,
 				.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED,
--- src/rhi/vulkan/swapchain.cpp
+++ src/rhi/vulkan/swapchain.cpp
@@ -173,7 +173,7 @@ void Swapchain<kVk>::InternalCreateSwapchain(
 	info.imageColorSpace = config.surfaceFormat.colorSpace;
 	info.imageExtent = config.extent;
 	info.imageArrayLayers = 1;
-	info.imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT | VK_IMAGE_USAGE_STORAGE_BIT/* | VK_IMAGE_USAGE_TRANSFER_DST_BIT*/;
+	info.imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT | VK_IMAGE_USAGE_STORAGE_BIT | VK_IMAGE_USAGE_TRANSFER_DST_BIT;
 	info.imageSharingMode = VK_SHARING_MODE_EXCLUSIVE;
 	info.preTransform = VK_SURFACE_TRANSFORM_IDENTITY_BIT_KHR;
 	info.compositeAlpha = VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR;
