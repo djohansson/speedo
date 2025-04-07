babf290f064ab14efdc340b7d85de41e5dbf1d63
Daniel Johansson <1911831+djohansson@users.noreply.github.com>
2025-04-07 23:26:42 +0200

    minor opt

--- src/rhi/vulkan/window.cpp
+++ src/rhi/vulkan/window.cpp
@@ -33,16 +33,17 @@ void Window<kVk>::InternalUpdateViewBuffer() const
 	for (uint32_t viewIt = 0UL; viewIt < viewCount; viewIt++)
 	{
 		auto mvp = cameras.Get()[viewIt].GetProjectionMatrix() * cameras.Get()[viewIt].GetViewMatrix();
+		auto* vpDst = viewDataPtr->viewProjection;
 #if (GLM_ARCH & GLM_ARCH_AVX_BIT) && (SPEEDO_GRAPHICS_VALIDATION_LEVEL <= 1)
-		_mm256_stream_ps(&viewDataPtr->viewProjection[0][0], _mm256_insertf128_ps(_mm256_castps128_ps256(mvp[0].data), mvp[1].data, 1));
-		_mm256_stream_ps(&viewDataPtr->viewProjection[2][0], _mm256_insertf128_ps(_mm256_castps128_ps256(mvp[2].data), mvp[3].data, 1));
+		_mm256_stream_ps(&vpDst[0][0], _mm256_insertf128_ps(_mm256_castps128_ps256(mvp[0].data), mvp[1].data, 1));
+		_mm256_stream_ps(&vpDst[2][0], _mm256_insertf128_ps(_mm256_castps128_ps256(mvp[2].data), mvp[3].data, 1));
 #elif (GLM_ARCH & GLM_ARCH_SSE2_BIT) && (SPEEDO_GRAPHICS_VALIDATION_LEVEL <= 1)
-		_mm_stream_ps(&viewDataPtr->viewProjection[0][0], mvp[0].data);
-		_mm_stream_ps(&viewDataPtr->viewProjection[1][0], mvp[1].data);
-		_mm_stream_ps(&viewDataPtr->viewProjection[2][0], mvp[2].data);
-		_mm_stream_ps(&viewDataPtr->viewProjection[3][0], mvp[3].data);
+		_mm_stream_ps(&vpDst[0][0], mvp[0].data);
+		_mm_stream_ps(&vpDst[1][0], mvp[1].data);
+		_mm_stream_ps(&vpDst[2][0], mvp[2].data);
+		_mm_stream_ps(&vpDst[3][0], mvp[3].data);
 #else
-		std::copy_n(&mvp[0][0], 16, &viewDataPtr->viewProjection[0][0]);
+		std::copy_n(&mvp[0][0], 16, &vpDst[0][0]);
 #endif
 		viewDataPtr++;
 	}
