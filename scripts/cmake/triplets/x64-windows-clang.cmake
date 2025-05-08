d418a7b1c7a85d25465f192f5d7addc048ea1c83
Daniel Johansson <1911831+djohansson@users.noreply.github.com>
2025-05-09 00:19:50 +0200

    move setup to cmake presets

--- scripts/cmake/triplets/x64-windows-clang.cmake
+++ scripts/cmake/triplets/x64-windows-clang.cmake
@@ -3,10 +3,3 @@ set(VCPKG_VISUAL_STUDIO_PATH $ENV{VISUAL_STUDIO_PATH})
 
 set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE dynamic)
-
-set(VCPKG_ENV_PASSTHROUGH PATH LLVM_PATH LLVM_TOOLS_PATH)
-set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE ${CMAKE_CURRENT_LIST_DIR}/../toolchains/clang.toolchain.cmake)
-
-# VCPKG_LOAD_VCVARS_ENV is needed for libraries using vcpkg through visual studio as a build system (for example tracy).
-# must be set _after_ the toolchain file is loaded, since that will set VCPKG_LOAD_VCVARS_ENV to OFF.
-set(VCPKG_LOAD_VCVARS_ENV ON) 
