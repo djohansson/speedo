07362d70db6dd39083cd29f8b6539c35b9afc9c7
Daniel Johansson <1911831+djohansson@users.noreply.github.com>
2025-05-10 22:46:12 +0200

    cleanup - install windows sdk as part of vs build tools, remove ninja and cmake

--- scripts/platforms/windows/windows.ps1
+++ scripts/platforms/windows/windows.ps1
@@ -30,53 +30,20 @@ if (-not ($gitCmd))
 	$gitCmd = Get-Command "git" -All -ErrorAction SilentlyContinue | Where-Object Version -GE ([System.Version]"2.49.0.0")
 }
 
-$ninjaCmd = Get-Command "ninja" -All -ErrorAction SilentlyContinue
-if (-not ($ninjaCmd))
-{ 
-	Write-Host "Installing Ninja..."
-
-	Install-WinGetPackage -Mode Silent -Id Ninja-build.Ninja | Out-Null
-
-	$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 
-
-	$ninjaCmd = Get-Command "ninja" -All -ErrorAction SilentlyContinue
-}
-
-$cmakeCmd = Get-Command "cmake" -All -ErrorAction SilentlyContinue | Where-Object Version -GE ([System.Version]"3.31.6.0")
-if (-not ($cmakeCmd))
-{ 
-	Write-Host "Installing Cmake..."
-
-	Install-WinGetPackage -Mode Silent -Id Kitware.CMake | Out-Null
-
-	$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 
-
-	$cmakeCmd = Get-Command "cmake" -All -ErrorAction SilentlyContinue | Where-Object Version -GE ([System.Version]"3.31.6.0")
-}
-
-$windowsSdkInfo = Get-WinGetPackage Microsoft.WindowsSDK.10.0.26100
-if (-not ($windowsSdkInfo))
-{
-	Write-Host "Installing WindowsSDK 10.0.26100 (requires process elevation)..."
-
-	Start-Process pwsh -Verb runas -ArgumentList "-c Install-WinGetPackage -Mode Silent -Id Microsoft.WindowsSDK.10.0.26100 | Out-Null" -Wait
-
-	$windowsSdkInfo = Get-WinGetPackage Microsoft.WindowsSDK.10.0.26100
-}
-$winSDKManifest = [xml](Get-Content -Path "C:\Program Files (x86)\Windows Kits\10\SDKManifest.xml")
-$platformIndentityStr = $winSDKManifest.FileList.PlatformIdentity
-$windowsSdkVersion = $platformIndentityStr.SubString($platformIndentityStr.LastIndexOf("Version=") + 8)
-
-$VSSetupInstance = Get-VSSetupInstance | Select-VSSetupInstance -Product * -Require "Microsoft.VisualStudio.Workload.VCTools","Microsoft.VisualStudio.Component.VC.ATL","Microsoft.VisualStudio.Component.VC.Llvm.Clang"
+$VSSetupInstance = Get-VSSetupInstance | Select-VSSetupInstance -Product * -Require "Microsoft.VisualStudio.Workload.VCTools","Microsoft.VisualStudio.Component.VC.ATL","Microsoft.VisualStudio.Component.VC.Llvm.Clang","Microsoft.VisualStudio.Component.Windows11SDK.22621"
 if (-not ($VSSetupInstance))
 {
 	Write-Host "Installing VisualStudio 2022 VC BuildTools..."
 
 	# --force is required to circumvent the fact that Microsoft.VisualStudio.2022.BuildTools could already be installed without the VCTools workload
-	winget install -e -h --id Microsoft.VisualStudio.2022.BuildTools --override "--quiet --add Microsoft.VisualStudio.Workload.VCTools;includeRecommended --add Microsoft.VisualStudio.Component.VC.ATL --add Microsoft.VisualStudio.Component.VC.Llvm.Clang --wait" --force
+	winget install -e -h --id Microsoft.VisualStudio.2022.BuildTools --override "--quiet --add Microsoft.VisualStudio.Workload.VCTools;includeRecommended --add Microsoft.VisualStudio.Component.VC.ATL --add Microsoft.VisualStudio.Component.VC.Llvm.Clang --add Microsoft.VisualStudio.Component.Windows11SDK.22621 --wait" --force
 
-	$VSSetupInstance = Get-VSSetupInstance | Select-VSSetupInstance -Product * -Require "Microsoft.VisualStudio.Workload.VCTools"
+	$VSSetupInstance = Get-VSSetupInstance | Select-VSSetupInstance -Product * -Require "Microsoft.VisualStudio.Workload.VCTools","Microsoft.VisualStudio.Component.VC.ATL","Microsoft.VisualStudio.Component.VC.Llvm.Clang","Microsoft.VisualStudio.Component.Windows11SDK.22621"
 }
+
+$winSDKManifest = [xml](Get-Content -Path "C:\Program Files (x86)\Windows Kits\10\SDKManifest.xml")
+$platformIndentityStr = $winSDKManifest.FileList.PlatformIdentity
+$windowsSdkVersion = $platformIndentityStr.SubString($platformIndentityStr.LastIndexOf("Version=") + 8)
 
 $global:myEnv | Add-Member -Force -PassThru -NotePropertyName POWERSHELL_PATH -NotePropertyValue (Split-Path -Path $pwshCmd.Source) | Out-Null
 $global:myEnv | Add-Member -Force -PassThru -NotePropertyName GIT_PATH -NotePropertyValue (Split-Path -Path $gitCmd.Source) | Out-Null
