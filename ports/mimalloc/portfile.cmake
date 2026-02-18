234aede8c9c8991937c1a288355b17bb64c02f9b
Daniel Johansson <1911831+djohansson@users.noreply.github.com>
2026-02-18 17:46:40 +0100

    update mimalloc port

--- ports/mimalloc/portfile.cmake
+++ ports/mimalloc/portfile.cmake
@@ -1,59 +1,58 @@
 vcpkg_from_github(
-  OUT_SOURCE_PATH SOURCE_PATH
-  REPO microsoft/mimalloc
-  HEAD_REF dev2
-  REF v${VERSION}
-  SHA512 c5361881db58a9a02904fd90e32f5c58ff9590a5983f5125163e875d47699936b8550c283e50e3d9c18ebc37404dc18ce5646a9381f7057c57324cf7f76ffd95
-  PATCHES
-    0001-clang-fix.patch
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO microsoft/mimalloc
+    REF "v${VERSION}"
+    SHA512 55262050f63868e3029cd929a74d312dc0f34b606534b1d0b3735eecc8eed68aae97523a50228b4ac4044e1e03192f2909440e3a27607e2d364607ac0bda828f
+    HEAD_REF master
+    PATCHES
+        pkgconfig-cxx.diff
 )
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
-  FEATURES
-    c           MI_NO_USE_CXX
-    guarded     MI_GUARDED
-    secure      MI_SECURE
-    override    MI_OVERRIDE
-    #optarch     MI_OPT_ARCH
-    optsimd     MI_OPT_SIMD
-    xmalloc     MI_XMALLOC
-    asm         MI_SEE_ASM
+    FEATURES
+        override    MI_OVERRIDE
+        secure      MI_SECURE
 )
 string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "static" MI_BUILD_STATIC)
 string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" MI_BUILD_SHARED)
 
 vcpkg_cmake_configure(
-  SOURCE_PATH "${SOURCE_PATH}"
-  OPTIONS
-    -DMI_USE_CXX=ON
-    -DMI_BUILD_TESTS=OFF
-    -DMI_BUILD_OBJECT=ON
-    -DMI_BUILD_STATIC=${MI_BUILD_STATIC}
-    -DMI_BUILD_SHARED=${MI_BUILD_SHARED}
-    -DMI_INSTALL_TOPLEVEL=ON
-    ${FEATURE_OPTIONS}
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        ${FEATURE_OPTIONS}
+        -DMI_USE_CXX=OFF
+        -DMI_BUILD_OBJECT=OFF
+        -DMI_BUILD_TESTS=OFF
+        -DMI_BUILD_STATIC=${MI_BUILD_STATIC}
+        -DMI_BUILD_SHARED=${MI_BUILD_SHARED}
+        -DMI_INSTALL_TOPLEVEL=ON
+    OPTIONS_DEBUG
+        -DMI_DEBUG_FULL=ON
+    OPTIONS_RELEASE
+        -DMI_DEBUG_FULL=OFF
 )
 
 vcpkg_cmake_install()
+
 vcpkg_copy_pdbs()
 
 file(COPY
-  "${CMAKE_CURRENT_LIST_DIR}/vcpkg-cmake-wrapper.cmake"
-  "${CMAKE_CURRENT_LIST_DIR}/usage"
-  DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
+    "${CMAKE_CURRENT_LIST_DIR}/vcpkg-cmake-wrapper.cmake"
+    "${CMAKE_CURRENT_LIST_DIR}/usage"
+    DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
 )
 vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/mimalloc)
 
 if(VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
-  # todo: why is this needed?
-  vcpkg_replace_string(
-    "${CURRENT_PACKAGES_DIR}/include/mimalloc.h"
-    "!defined(MI_SHARED_LIB)"
-    "0 // !defined(MI_SHARED_LIB)"
-  )
+    vcpkg_replace_string(
+        "${CURRENT_PACKAGES_DIR}/include/mimalloc.h"
+        "!defined(MI_SHARED_LIB)"
+        "0 // !defined(MI_SHARED_LIB)"
+    )
 endif()
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 
 vcpkg_fixup_pkgconfig()
+
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
--- ports/mimalloc/vcpkg-cmake-wrapper.cmake
+++ ports/mimalloc/vcpkg-cmake-wrapper.cmake
@@ -1,16 +1,21 @@
 _find_package(${ARGS})
 
-if(CMAKE_CURRENT_LIST_DIR STREQUAL ${MIMALLOC_CMAKE_DIR}/${MIMALLOC_VERSION_DIR})
-    set(MIMALLOC_INCLUDE_DIR ${VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/include)
+if(CMAKE_CURRENT_LIST_DIR STREQUAL "${MIMALLOC_CMAKE_DIR}/${MIMALLOC_VERSION_DIR}")
+    set(MIMALLOC_INCLUDE_DIR "${VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/include")
     # As in vcpkg.cmake
-    set(MIMALLOC_LIBRARY_DIR ${VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/$<IF:$<CONFIG:Debug>,debug/lib,lib>)
-    set(MIMALLOC_OBJECT_DIR ${MIMALLOC_LIBRARY_DIR})
-    set(MIMALLOC_TARGET_DIR ${MIMALLOC_LIBRARY_DIR})
+    if(NOT DEFINED CMAKE_BUILD_TYPE OR CMAKE_BUILD_TYPE MATCHES "^[Dd][Ee][Bb][Uu][Gg]$")
+        set(MIMALLOC_LIBRARY_DIR "${VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/debug/lib")
+    else()
+        set(MIMALLOC_LIBRARY_DIR "${VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/lib")
+    endif()
+    set(MIMALLOC_OBJECT_DIR "MIMALLOC_OBJECT_DIR-NOTFOUND") # not installed
+    set(MIMALLOC_TARGET_DIR "${MIMALLOC_LIBRARY_DIR}")
 endif()
 
-# vcpkg always configures either a static or dynamic library.
-# ensure to always expose the mimalloc target as either the static or dynamic build.
-if(TARGET mimalloc-static AND NOT TARGET mimalloc)
-  add_library(mimalloc INTERFACE IMPORTED)
-  set_target_properties(mimalloc PROPERTIES INTERFACE_LINK_LIBRARIES mimalloc-static)
+if(TARGET mimalloc AND NOT TARGET mimalloc-static)
+    add_library(mimalloc-static INTERFACE IMPORTED)
+    set_target_properties(mimalloc-static PROPERTIES INTERFACE_LINK_LIBRARIES mimalloc)
+elseif(TARGET mimalloc-static AND NOT TARGET mimalloc)
+    add_library(mimalloc INTERFACE IMPORTED)
+    set_target_properties(mimalloc PROPERTIES INTERFACE_LINK_LIBRARIES mimalloc-static)
 endif()
