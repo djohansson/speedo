2201448a5889e4de56a2ac0fb9166f47a8399cb2
Daniel Johansson <1911831+djohansson@users.noreply.github.com>
2026-03-20 16:49:34 +0100

    add fastbuild patches to shader-slang port

--- ports/shader-slang/portfile.cmake
+++ ports/shader-slang/portfile.cmake
@@ -3,6 +3,11 @@ vcpkg_from_github(
 	REPO djohansson/slang
 	REF a43b4e194a8788ce77c2e59216343943cb25ad4f
 	SHA512 8b086295b682e1020d95220ad191fea393fd8314bbab4e72bbb25b724b9842317dd007bb3a3737396c066df1b2c5bb09d87dcb4900aefd8592ba6f554442921f
+	PATCHES
+		0001-fix-fastbuild-duplicate-neural-shader-targets.patch
+		0002-fix-fastbuild-missing-capability-defs-target.patch
+		0003-fix-fastbuild-capability-generator-build-order.patch
+		0004-fix-fastbuild-static-build-glsl-module-install.patch
 	HEAD_REF vcpkg-integration
 )
 
@@ -11,6 +16,9 @@ vcpkg_cmake_configure(
 	OPTIONS
 		-DSLANG_VERSION_FULL=${VERSION}
 		-DSLANG_VERSION_NUMERIC=${VERSION}
+		-DSLANG_LIB_TYPE=STATIC
+		-DSLANG_EMBED_CORE_MODULE=OFF
+		-DSLANG_EMBED_CORE_MODULE_SOURCE=ON
 		-DSLANG_SLANG_LLVM_FLAVOR=DISABLE
 		-DSLANG_ENABLE_GFX=OFF
 		-DSLANG_ENABLE_DXIL=OFF
